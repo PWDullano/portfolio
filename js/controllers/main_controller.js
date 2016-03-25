b100c53267fcae1fa9e82819cb36da427c1e5f36
Patrick Dullano <PWDullano@gmail.com>
2016-03-24 23:12:16 -0600

    initial commit for second design

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,3 +1,12 @@
 app.controller('mainController', ['$scope', '$http', function($scope, $http){
+  $scope.showProjects = false;
+  $scope.showAbout = false;
 
+ $scope.ToggleProjects = function(){
+   $scope.showProjects = !$scope.showProjects;
+ }
+
+ $scope.ToggleAbout = function(){
+   $scope.showAbout = !$scope.showAbout;
+ }
 }])
